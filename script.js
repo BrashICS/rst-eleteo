8324e7bd028021da9fcc73d579535e25ccec41f5
eleteo <ele.teo@stu.ocsb.ca>
2024-06-06 09:45:30 -0400

    help

--- script.js
+++ script.js
@@ -19,22 +19,43 @@ let score = 0;
 let fr = 5;
 let lastMilestone = 0;
 let hs = 0;
+let mode = "start";
+let playB;
+let resetB;
+
 
 function setup() {
-
+  playB = createButton('Play');
+  playB.size(150, 35);
+  playB.style("font-family", "Courier New");
+  playB.style("font-size", "20px");
+  playB.position(width/2-64,height/2+30);
   createCanvas(width,height);
+  playB.mousePressed(startGame);
+  setVariables();
+
+}
+
+function setVariables(){
   widthScale = width/scaleGrid;
   heightScale = height/scaleGrid;
   frameRate(fr);
   snake = new Snake();
   setFoodloc();
 }
+
 function setFoodloc() {
   let x = floor(random(widthScale));
   let y = floor(random(heightScale));
   food = createVector(x, y);
 
 }
+
+function startGame() {
+  mode = "play";
+  playB.remove();
+}
+
 function keyPressed() { // (0,0) is at top left
   if (keyCode === RIGHT_ARROW) {
     snake.setDir(1,0);
@@ -48,57 +69,72 @@ function keyPressed() { // (0,0) is at top left
   else if (keyCode === UP_ARROW) {
     snake.setDir(0,-1);
   }
-  else if (keyCode === 32){
-    resetGame();
-  }
 }
 
 function draw() {
-
-  scale(scaleGrid);
   background(231,233,150);
-  fill(151,162,117)
-  textAlign(CENTER)
-  textSize(8);
-  text(score, floor(widthScale/2),floor(heightScale/2)+2);
-  if (score > 0 && score % 5 === 0 && score !== lastMilestone) {
-    fr += 2;
-    frameRate(fr);
-    lastMilestone = score;
+  if (mode == "play") {
+    scale(scaleGrid);
+
+    fill(151,162,117)
+    textAlign(CENTER)
+    textSize(8);
+    text(score, floor(widthScale/2),floor(heightScale/2)+2);
+    if (score > 0 && score % 5 === 0 && score !== lastMilestone) {
+      fr += 2;
+      frameRate(fr);
+      lastMilestone = score;
+    }
+    if (snake.eat(food)==true) {
+      setFoodloc();
+      score +=1;
+    }
+    snake.update()
+    snake.display();
+    noStroke();
+    fill(60 )
+    rect(food.x, food.y, 1, 1);
+    if (snake.gameOver()==true){
+      gameOverScreen();
+    }
   }
-  if (snake.eat(food)==true) {
-    setFoodloc();
-    score +=1;
+  else if (mode == "gameover"){
+    background(231,233,150);
+    fill(60)
+    textSize(3);
+    text('GAME OVER', floor(widthScale/2),12);
+    textSize(1.2);
+    fill(89,94,80)
+    if (score>hs) {hs=score}
+    text('YOUR SCORE: '+score+'\nHIGH SCORE: '+hs, floor(widthScale/2), floor(heightScale/2+6));
+    resetB = createButton('Play Again');
+    resetB.size(150, 35);
+    resetB.style("font-family", "Courier New");
+    resetB.style("font-size", "20px");
+    resetB.position(width/2-60,height/2);
+    createCanvas(width,height);
+    resetB.mousePressed(resetGame);
   }
-  snake.update()
-  snake.display();
-  noStroke();
-  fill(60 )
-  rect(food.x, food.y, 1, 1);
-  if (snake.gameOver()==true){
-    noLoop();
-    gameOverScreen();
+  else if (mode=="start"){
+    textFont('Courier New');
+    textAlign(CENTER)
+    fill(60)
+    textSize(45);
+    text('SNAKE GAME', width/2,height/2-20);
   }
 }
 
 function gameOverScreen() {
-  background(231,233,150);
-  textFont('Courier New');
-  fill(60)
-  textSize(3);
-  text('GAME OVER', floor(widthScale/2),12);
-  textSize(1);
-  fill(89,94,80)
-  text(' PRESS SPACE \nTO PLAY AGAIN', floor(widthScale/2), 15);
-  if (score>hs) {hs=score}
-  text('HIGH SCORE:'+hs, floor(widthScale/2), floor(heightScale/2+5));
+  mode = "gameover";
+
 }
 
 function resetGame() {
+  resetB.remove();
   fr = 5;
   score = 0;
-  setup();
-  loop();
+  mode = "play"
+  setVariables();
 }
 
 class Snake {
